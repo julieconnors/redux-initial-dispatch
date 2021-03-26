e468d63ea58c188254bfef4133dc25c806475256
julieconnors <juliana.g.connors@gmail.com>
2021-03-26 06:28:47 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,28 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function reducer(state = { count: 0 }, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
   }
+} 
 
 function dispatch(action){
-    state = changeState(state, action)
+    state = reducer(state, action)
     render()
 }
 
-function render(){
-    document.body.textContent = state.count
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = state.count;
 }
+
+let button = document.getElementById('button')
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+
+dispatch({ type: '@@INIT' }) 
